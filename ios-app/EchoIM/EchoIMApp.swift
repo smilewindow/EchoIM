6267e0859c1fdbca2d8fefb7ae59c36bfccd9290
smilewindow <michaelxuyuqin@163.com>
2026-04-16 21:14:39 -0700

    feat: add ios app scaffold

---
+++ ios-app/EchoIM/EchoIMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoIMApp.swift
+//  EchoIM
+//
+//  Created by 许宇勤 on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoIMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
